442a1607e788aa529263509f9b202436b09d56b5
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2026-05-03 13:34:42 +0300

    Ue scoped CSS

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
-import "./Footer.css";
+import styles from "./Footer.module.css";
 
 export default function Footer() {
   return (
-    <div className="footer">&copy; 2026 Face Grid. All rights reserved</div>
+    <div className={styles.footer}>&copy; 2026 Face Grid. All rights reserved</div>
   )
 }
